51dfcc840c0a4cdb05e22a6f70ba8ea4be99d7cc
Teferi Assefa <teferi.assefa@gmail.com>
2017-12-05 14:16:37 +0000

    Update on Dockerfile and build script

--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,7 @@
 # Dockerfile for bidir  web admin service
-# This image will be based on the official nodejs docker image
-FROM node:latest
+FROM nginx
 
-# Set in what directory commands will run
-WORKDIR /
+WORKDIR /usr/share/nginx/html
 
-# Put all our code inside that directory that lives in the container
-ADD . /
-
-# Install dependencies
-
-# Tell Docker we are going to use this port
-EXPOSE 9000
-
-# The command to run our app when the container is run
-CMD ["node", "app.js"]
+COPY . /usr/share/nginx/html
+EXPOSE 18080
--- build.sh
+++ build.sh
@@ -2,21 +2,21 @@
 DOCKER_PATH=$(which docker)
 SERVICE_NAME=bidir-web
 IMAGE_TAG=bidir/$SERVICE_NAME
-EXPOSE_PORT=80
+EXPOSE_PORT=18080
 CONT_PORT=80
-HOST_IP=10.142.0.2
-MONGODB_URL=mongodb://10.142.0.2:27017/bidir
+HOST_IP=10.142.0.5
+MONGODB_URL=mongodb://10.142.0.5:27017/bidir
+cd master
 NPM=$(which npm)
+sudo $NPM install
 sudo $NPM install -g gulp
-sudo $NPM install bower
-BOWER=$(which bower)
-$BOWER install --root-access
-sudo $NPM install gulp 
+sudo $NPM install bower install
+sudo $NPM install gulp
 sudo $NPM install node-build-web-app --save-dev
-sudo $NPM install --no-optional 
+sudo $NPM install --no-optional
 GULP=$(which gulp)
-$GULP build 
-
+$GULP build
+cd ..
 #Stop running container
 $DOCKER_PATH stop $SERVICE_NAME
 # Remove container
@@ -30,8 +30,6 @@ $DOCKER_PATH run -d \
   --name $SERVICE_NAME \
   -p $HOST:$EXPOSE_PORT:$CONT_PORT \
   -e HOST=$HOST_IP \
-  -e API_URL=$API_URL \
   -e MONGODB_URL=$MONGODB_URL \
   --restart=always \
   $IMAGE_TAG
-
