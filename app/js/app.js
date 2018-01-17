283ae77a6ff017af82f96223b7891ad815e6b082
Yonas Tesfaye <ytesfayee@gmail.com>
2018-01-17 06:22:55 +0300

    change on buildPaginatedUrl

--- app/js/app.js
+++ app/js/app.js
@@ -4150,63 +4150,6 @@ var API = {
         .factory('CommonService', CommonService);
 
     function CommonService() {
-      var validationObject = {
-        ComputeValidation: function (validationObject) {
-            var isValid = true;
-            var properties = _.allKeys(validationObject);
-            _.each(properties, function (property) {
-                isValid = isValid && validationObject[property];
-            });
-            return isValid;
-        },
-        ResetValidationObject: function (validationObject) {
-            var properties = _.allKeys(validationObject);
-            _.each(properties, function (property) {
-                validationObject[property] = true;
-            });
-        },
-        ValidateForm: function (validationObject, referenceObject) {
-            var isValid = true;
-            var properties = _.allKeys(validationObject);
-            var validateSingleObject= function (objValue) {
-                if(_.isString(objValue)){
-                    return !_.isUndefined(objValue) && !_.isNull(objValue) && objValue.length > 0;
-                }
-
-                if(_.isNumber(objValue)){
-                    return !_.isUndefined(objValue) && !_.isNull(objValue) && !_.isNaN(objValue);
-                }
-
-                if(_.isDate(objValue)){
-                    return !_.isUndefined(objValue) && !_.isNull(objValue);
-                }
-
-                if(_.isObject(objValue)){
-
-                    return !_.isUndefined(objValue) && !_.isNull(objValue)&& !_.isEmpty(objValue);
-                }
-
-                return !_.isUndefined(objValue) && !_.isNull(objValue);
-            };
-
-            _.each(properties, function (property) {
-                var index = property.indexOf('Valid');
-                if(index != -1) {
-                    var objProperty = property.substring(2, index);
-                }
-                if (_.has(referenceObject, objProperty)) {
-                    var objValue = referenceObject[objProperty];
-                    validationObject[property] = validateSingleObject(objValue);
-                }else{
-                    console.log('Validation failed for: ',objProperty);
-                    validationObject[property] = false;
-                    isValid = false;
-                }
-                isValid = isValid && validationObject[property];
-            });
-            return isValid;
-        }
-    };
         var factory = {
             buildUrl: _buildUrl,
             buildPaginatedUrl:_buildPaginatedUrl,
@@ -4278,10 +4221,8 @@ var API = {
           return API.Config.BaseUrl + service +'/' + url;
         }
         function _buildPaginatedUrl(service,url,params) {
-            var parameters = {start:1,limit:100};
-            return url===''?API.Config.BaseUrl + service + '/paginate?page='+parameters.start+'&per_page=' + parameters.limit:
-                API.Config.BaseUrl + service +'/' + url + '/paginate?page='+parameters.start+'&per_page=' + parameters.limit;
-
+            return url===''?API.Config.BaseUrl + service + '/paginate':
+                API.Config.BaseUrl + service +'/' + url + '/paginate';
         }
 
         function _buildUrlWithParam(service,url, id) {
