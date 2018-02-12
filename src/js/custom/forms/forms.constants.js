80330b1ca88aec50f7ef3974b88a3501f0ff9dd5
Yonas Tesfaye <ytesfayee@gmail.com>
2018-02-12 03:03:16 +0300

    display name changed for Question Type

--- src/js/custom/forms/forms.constants.js
+++ src/js/custom/forms/forms.constants.js
@@ -9,9 +9,9 @@
         .constant('MW_QUESTION_TYPES', [
             {name:'Fill In Blank',url:'fib',code:'FILL_IN_BLANK',type:'text'},
             {name:'Yes/No',code:'yn',url:'yn',type:'YES_NO',options:['Yes','No']},
-            {name:'MULTIPLE_CHOICE',url:'mc',code:'MULTIPLE_CHOICE',type:'checkbox'},
-            {name:'SINGLE_CHOICE',url:'sc',code:'SINGLE_CHOICE',type:'select'},
-            {name:'GROUPED',url:'GROUPED',code:'GROUPED',type:''}])
+            {name:'Multiple Choice',url:'mc',code:'MULTIPLE_CHOICE',options:[],type:'checkbox'},
+            {name:'Single Choice',url:'sc',code:'SINGLE_CHOICE',options:[],type:'select'},
+            {name:'Grouped',url:'GROUPED',code:'GROUPED',type:''}])
         .constant('MW_FORM_TYPES', [
             {name:'ACAT',code:'ACAT'},
             {name:'LOAN APPLICATION',code:'LOAN_APPLICATION'},
