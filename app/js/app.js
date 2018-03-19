8799261f593008af617759838e568c9968ffd090
Yonas Tesfaye <ytesfayee@gmail.com>
2018-03-19 05:24:27 +0300

    loan product UI

--- app/js/app.js
+++ app/js/app.js
@@ -5286,11 +5286,39 @@ function runBlock() {
 
     angular.module("app.acat").controller("LoanProductsController", LoanProductsController);
 
-    LoanProductsController.$inject = ['$state'];
+    LoanProductsController.$inject = ['$mdDialog','RouteHelpers'];
 
-    function LoanProductsController($state) {
+    function LoanProductsController($mdDialog,RouteHelpers) {
+        var vm = this;
+        vm.addLoanProduct = _addLoanProduct;
+        vm.editLoanProduct = _editLoanProduct;
 
-        console.log("LoanProducts Controller");
+
+        function _addLoanProduct(loan_product,ev) {
+            $mdDialog.show({
+                locals: {data:{loan_product:loan_product}},
+                templateUrl: RouteHelpers.basepath('acat/loanproduct/loan.product.dialog.html'),
+                parent: angular.element(document.body),
+                targetEvent: ev,
+                clickOutsideToClose: false,
+                hasBackdrop: false,
+                escapeToClose: true,
+                controller: LoanProductDialogController,
+                controllerAs: 'vm'
+            }).then(function (answer) {
+
+            }, function (response) {
+                console.log("refresh on response");
+            });
+        }
+        function _editLoanProduct(item,ev) {
+
+        }
+
+
+        function LoanProductDialogController() {
+
+        }
     }
 
 
