3b3ed549d2f4566f21e5b10c53b88dc42bb798b4
Yonas Tesfaye <ytesfayee@gmail.com>
2018-06-20 11:33:28 +0300

    Crop ACAT template added

--- src/js/custom/loan_management/loan_processing/acat/acat.processor.controller.js
+++ src/js/custom/loan_management/loan_processing/acat/acat.processor.controller.js
@@ -17,29 +17,23 @@
         vm.cropOptions = [ "Tomato","Onion","Cabbage",'Bean','Crop 1'];
         vm.paginate = _paginate;
         vm.clearSearchText = _clearSearchText;
+
+        vm.addEditClientACAT = _addEditClientACAT;
         //CLIENT ACAT
         vm.onClientACATClick = _onClientACATClick;
-
         initialize();
 
         function initialize() {
-            vm.visibility = { showClientACAT:false };
+            vm.visibility = {
+                showClientACAT:false,
+                showCropACAT:false
+            };
             vm.options =   MD_TABLE_GLOBAL_SETTINGS.OPTIONS;
             vm.filter = {show : false};
             vm.pageSizes = MD_TABLE_GLOBAL_SETTINGS.PAGE_SIZES;
 
             vm.query = { search:'',   page:1,  per_page:10 };
-            fetchCropsList();
             callAPI();
-        }
-
-        function fetchCropsList() {
-            LoanManagementService.GetCrops().then(
-                function (response) {
-                    console.log("response.data",response);
-                    vm.crops = response.data.docs;
-                }
-            )
         }
 
         function callAPI() {
@@ -55,23 +49,15 @@
 
         function _onClientACATClick(acat, ev) {
             vm.visibility.showClientACAT = true;//show client acat
+            vm.visibility.showCropACAT = false;
             vm.acats = acat;
-            console.log("loan product",acat);
-            vm.acat = vm.acats.ACATs[0].sections[0];
-            vm.selectedSubsection = vm.acat.sub_sections[0].sub_sections[1];
-            vm.acats.crops = [];
-            _.each(vm.acats.ACATs,function (acat_crop) {
-                vm.acats.crops.push(acat_crop.crop);
-            });
         }
-
 
 
         function _paginate(page, pageSize) {
             vm.query.page = page;
             vm.query.per_page = pageSize;
             callAPI();
-
         }
         function _clearSearchText() {
             vm.query.search = '';
@@ -86,19 +72,16 @@
             }
         });
 
-
-
-
-        vm.onSubsectionClick = function(subsection) {
-            vm.toggle[subsection._id] = !vm.toggle[subsection._id];
-            if (subsection.sub_sections.length == 0) {
-                vm.selectedSubsection = subsection;
-            }
-        };
-
-        vm.onAccordionClick = function(acc) {
-            vm.toggle[acc._id] = !vm.toggle[acc._id];
-        };
+        function _addEditClientACAT(clientAcat) {
+            vm.visibility.showCropACAT = true;//show client acat
+            console.log("Client ACAT Detail",clientAcat);
+            vm.acat = vm.acats.ACATs[0].sections[0];
+            vm.selectedSubsection = vm.acat.sub_sections[0].sub_sections[1];
+            vm.acats.crops = [];
+            _.each(vm.acats.ACATs,function (acat_crop) {
+                vm.acats.crops.push(acat_crop.crop);
+            });
+        }
 
 
 
