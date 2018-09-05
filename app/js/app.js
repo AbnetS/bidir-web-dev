52df34377a7cd6bb4ac30bc3b2fbf9de7044d23c
Yonas Tesfaye <ytesfayee@gmail.com>
2018-09-05 09:19:31 +0300

    baseUrl changed to dev

--- app/js/app.js
+++ app/js/app.js
@@ -3744,8 +3744,8 @@ function runBlock() {
 })(window.angular);
 var API = {
     Config: {
-        BaseUrl: 'http://api.buusaa-bidir.local/' //REMOTE APIs
-        // BaseUrl: 'http://api.dev.bidir.gebeya.co/' //REMOTE API
+        // BaseUrl: 'http://api.buusaa-bidir.local/' //REMOTE APIs
+        BaseUrl: 'http://api.dev.bidir.gebeya.co/' //REMOTE API
     },
     Service: {
         NONE:'',
@@ -8320,6 +8320,128 @@ var CIVIL_STATUSES  = ["single","married","widowed","other"];
 })(window.angular);
 
 /**
+ * Created by Yonas on 4/27/2018.
+ */
+(function(angular) {
+    "use strict";
+
+    angular.module("app.processing")
+        .controller("ACATProcessorController", ACATProcessorController);
+
+    ACATProcessorController.$inject = ['LoanManagementService','$scope'];
+
+    function ACATProcessorController(LoanManagementService,$scope ) {
+        var vm = this;
+        vm.selectedSubsection = {};
+        vm.toggle = {};
+        vm.accordionToggle = {};
+
+        vm.paginate = _paginate;
+        vm.clearSearchText = _clearSearchText;
+
+        vm.addEditClientACAT = _addEditClientACAT;
+        //CLIENT ACAT
+        vm.onClientACATClick = _onClientACATClick;
+        vm.onSubsectionClick = _onSubsectionClick;
+        vm.onAccordionClick = _onAccordionClick;
+
+        initialize();
+
+        function initialize() {
+            vm.visibility = {
+                showClientACAT:false,
+                showCropACAT:false
+            };
+            vm.options =   MD_TABLE_GLOBAL_SETTINGS.OPTIONS;
+            vm.filter = {show : false};
+            vm.pageSizes = MD_TABLE_GLOBAL_SETTINGS.PAGE_SIZES;
+
+            vm.query = { search:'',   page:1,  per_page:10 };
+            vm.months = MONTHS_CONST;
+            callAPI();
+            fetchCropsList();
+        }
+
+        function callAPI() {
+            vm.acatCollectionPromise = LoanManagementService
+                .GetACATCollections(vm.query).then(
+                function (response) {
+                    vm.acatCollection = response.data.docs;
+                    vm.query.total_docs_count = response.data.total_docs_count;
+                },function (error) {
+                        console.log("error",error);
+                    });
+        }
+
+
+
+        function _onSubsectionClick(subsection) {
+            vm.toggle[subsection._id] = !vm.toggle[subsection._id];
+            if (subsection.sub_sections.length === 0) {
+                vm.selectedSubsection = subsection;
+            }
+        }
+
+
+        function _onAccordionClick(acc) {
+            vm.toggle[acc._id] = !vm.toggle[acc._id];
+        }
+
+
+        function _onClientACATClick(acat, ev) {
+            vm.visibility.showClientACAT = true;//show client acat
+            vm.visibility.showCropACAT = false;
+            vm.acats = acat;
+        }
+
+
+        function _paginate(page, pageSize) {
+            vm.query.page = page;
+            vm.query.per_page = pageSize;
+            callAPI();
+        }
+        function _clearSearchText() {
+            vm.query.search = '';
+            vm.filter.show = false;
+        }
+
+        $scope.$watch(angular.bind(vm, function () {
+            return vm.query.search;
+        }), function (newValue, oldValue) {
+            if (newValue !== oldValue) {
+                console.log("search for ",newValue);
+            }
+        });
+
+        function _addEditClientACAT(clientAcat) {
+            vm.visibility.showCropACAT = true;//show client acat
+            console.log("Client ACAT Detail Clicked",clientAcat);
+            vm.clientAcat = clientAcat;
+            vm.clientAcat.loan_product = vm.acats.loan_product;
+
+            vm.selectedSubsection = vm.clientAcat.sections[0].sub_sections[0].sub_sections[1];
+
+        }
+
+
+        function fetchCropsList() {
+            LoanManagementService.GetCrops().then(
+                function (response) {
+                    console.log("vm.crops", response.data.docs);
+                    vm.crops = response.data.docs;
+                }
+            )
+        }
+
+
+
+
+    }
+
+
+
+})(window.angular);
+/**
  * Created by Yonas on 7/2/2018.
  */
 (function(angular) {
@@ -8486,128 +8608,6 @@ var CIVIL_STATUSES  = ["single","married","widowed","other"];
         });
 
     }
-
-})(window.angular);
-/**
- * Created by Yonas on 4/27/2018.
- */
-(function(angular) {
-    "use strict";
-
-    angular.module("app.processing")
-        .controller("ACATProcessorController", ACATProcessorController);
-
-    ACATProcessorController.$inject = ['LoanManagementService','$scope'];
-
-    function ACATProcessorController(LoanManagementService,$scope ) {
-        var vm = this;
-        vm.selectedSubsection = {};
-        vm.toggle = {};
-        vm.accordionToggle = {};
-
-        vm.paginate = _paginate;
-        vm.clearSearchText = _clearSearchText;
-
-        vm.addEditClientACAT = _addEditClientACAT;
-        //CLIENT ACAT
-        vm.onClientACATClick = _onClientACATClick;
-        vm.onSubsectionClick = _onSubsectionClick;
-        vm.onAccordionClick = _onAccordionClick;
-
-        initialize();
-
-        function initialize() {
-            vm.visibility = {
-                showClientACAT:false,
-                showCropACAT:false
-            };
-            vm.options =   MD_TABLE_GLOBAL_SETTINGS.OPTIONS;
-            vm.filter = {show : false};
-            vm.pageSizes = MD_TABLE_GLOBAL_SETTINGS.PAGE_SIZES;
-
-            vm.query = { search:'',   page:1,  per_page:10 };
-            vm.months = MONTHS_CONST;
-            callAPI();
-            fetchCropsList();
-        }
-
-        function callAPI() {
-            vm.acatCollectionPromise = LoanManagementService
-                .GetACATCollections(vm.query).then(
-                function (response) {
-                    vm.acatCollection = response.data.docs;
-                    vm.query.total_docs_count = response.data.total_docs_count;
-                },function (error) {
-                        console.log("error",error);
-                    });
-        }
-
-
-
-        function _onSubsectionClick(subsection) {
-            vm.toggle[subsection._id] = !vm.toggle[subsection._id];
-            if (subsection.sub_sections.length === 0) {
-                vm.selectedSubsection = subsection;
-            }
-        }
-
-
-        function _onAccordionClick(acc) {
-            vm.toggle[acc._id] = !vm.toggle[acc._id];
-        }
-
-
-        function _onClientACATClick(acat, ev) {
-            vm.visibility.showClientACAT = true;//show client acat
-            vm.visibility.showCropACAT = false;
-            vm.acats = acat;
-        }
-
-
-        function _paginate(page, pageSize) {
-            vm.query.page = page;
-            vm.query.per_page = pageSize;
-            callAPI();
-        }
-        function _clearSearchText() {
-            vm.query.search = '';
-            vm.filter.show = false;
-        }
-
-        $scope.$watch(angular.bind(vm, function () {
-            return vm.query.search;
-        }), function (newValue, oldValue) {
-            if (newValue !== oldValue) {
-                console.log("search for ",newValue);
-            }
-        });
-
-        function _addEditClientACAT(clientAcat) {
-            vm.visibility.showCropACAT = true;//show client acat
-            console.log("Client ACAT Detail Clicked",clientAcat);
-            vm.clientAcat = clientAcat;
-            vm.clientAcat.loan_product = vm.acats.loan_product;
-
-            vm.selectedSubsection = vm.clientAcat.sections[0].sub_sections[0].sub_sections[1];
-
-        }
-
-
-        function fetchCropsList() {
-            LoanManagementService.GetCrops().then(
-                function (response) {
-                    console.log("vm.crops", response.data.docs);
-                    vm.crops = response.data.docs;
-                }
-            )
-        }
-
-
-
-
-    }
-
-
 
 })(window.angular);
 /**
