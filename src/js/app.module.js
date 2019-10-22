558b27a25717f01508750d2db1d10add9da4132a
Yonas <vuvas57@gmail.com>
2019-10-22 16:53:27 +0300

    minor changes on UI

--- src/js/app.module.js
+++ src/js/app.module.js
@@ -1,13 +1,3 @@
-/*!
- * 
- * Angle - Bootstrap Admin App + AngularJS Material
- * 
- * Version: 3.8.1
- * Author: @themicon_co
- * Website: http://themicon.co
- * License: https://wrapbootstrap.com/help/licenses
- * 
- */
 
 // APP START
 // ----------------------------------- 
