510006c64daf22fc6767213338cd30c3216cbacd
Abnet Shimeles <abutihere@gmail.com>
2020-05-11 01:26:59 +0300

    remove src

--- src/js/custom/common/config.js
+++
@@ -1,6 +0,0 @@
-(function () { 
- return angular.module("app.common")
-.constant("EnvironmentConfig", {"BaseUrl":"http://api.dev.bidir.gebeya.co/","SeasonalMonitoringBaseUrl":"http://seasmon.wenr.wur.nl/cgi-bin/register.py?","SeasmonBaseUrl":"http://seasmon.wenr.wur.nl/html/"})
-.constant("app_version", "1.0");
-
-})();
